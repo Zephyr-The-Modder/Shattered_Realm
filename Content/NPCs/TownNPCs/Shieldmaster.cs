04870e14fedfc8883c07c221b1a40725d3788fc7
EnVy200 <zephyrlive6@gmail.com>
2024-03-30 18:55:17 -0500

    Shieldmaster (needs bugfix)

---
+++ Content/NPCs/TownNPCs/Shieldmaster.cs
@@ -0,0 +1,313 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Linq;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.Utilities;
+using Terraria.GameContent.Bestiary;
+using Terraria.GameContent.ItemDropRules;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.GameContent;
+using Terraria.GameContent.Personalities;
+using System.Collections.Generic;
+using ReLogic.Content;
+using Terraria.GameContent.UI;
+using Terraria.ModLoader.IO;
+using ShatteredRealm.Content.Globals;
+using ShatteredRealm.Content.Items.Accessories.Shield;
+namespace ShatteredRealm.Content.NPCs.TownNPCs
+{
+	// [AutoloadHead] and NPC.townNPC are extremely important and absolutely both necessary for any Town NPC to work at all.
+	[AutoloadHead]
+	public class Shieldmaster : ModNPC
+	{
+		public const string ShopName = "Shop";
+		public int NumberOfTimesTalkedTo = 0;
+
+		private static Profiles.StackedNPCProfile NPCProfile;
+
+
+		public override void SetStaticDefaults()
+		{
+			Main.npcFrameCount[Type] = 25; // The total amount of frames the NPC has
+
+			NPCID.Sets.ExtraFramesCount[Type] = 9; // Generally for Town NPCs, but this is how the NPC does extra things such as sitting in a chair and talking to other NPCs. This is the remaining frames after the walking frames.
+			NPCID.Sets.AttackFrameCount[Type] = 4; // The amount of frames in the attacking animation.
+			NPCID.Sets.DangerDetectRange[Type] = 700; // The amount of pixels away from the center of the NPC that it tries to attack enemies.
+			NPCID.Sets.AttackType[Type] = 0; // The type of attack the Town NPC performs. 0 = throwing, 1 = shooting, 2 = magic, 3 = melee
+			NPCID.Sets.AttackTime[Type] = 90; // The amount of time it takes for the NPC's attack animation to be over once it starts.
+			NPCID.Sets.AttackAverageChance[Type] = 30; // The denominator for the chance for a Town NPC to attack. Lower numbers make the Town NPC appear more aggressive.
+			NPCID.Sets.HatOffsetY[Type] = 4; // For when a party is active, the party hat spawns at a Y offset.
+			NPCID.Sets.ShimmerTownTransform[NPC.type] = true; // This set says that the Town NPC has a Shimmered form. Otherwise, the Town NPC will become transparent when touching Shimmer like other enemies.
+
+			NPCID.Sets.ShimmerTownTransform[Type] = true; // Allows for this NPC to have a different texture after touching the Shimmer liquid.
+
+			// Connects this NPC with a custom emote.
+			// This makes it when the NPC is in the world, other NPCs will "talk about him".
+			// By setting this you don't have to override the PickEmote method for the emote to appear.
+			
+
+			// Influences how the NPC looks in the Bestiary
+			NPCID.Sets.NPCBestiaryDrawModifiers drawModifiers = new NPCID.Sets.NPCBestiaryDrawModifiers()
+			{
+				Velocity = 1f, // Draws the NPC in the bestiary as if its walking +1 tiles in the x direction
+				Direction = 1 // -1 is left and 1 is right. NPCs are drawn facing the left by default but ExamplePerson will be drawn facing the right
+							  // Rotation = MathHelper.ToRadians(180) // You can also change the rotation of an NPC. Rotation is measured in radians
+							  // If you want to see an example of manually modifying these when the NPC is drawn, see PreDraw
+			};
+
+			NPCID.Sets.NPCBestiaryDrawOffset.Add(Type, drawModifiers);
+
+			// Set Example Person's biome and neighbor preferences with the NPCHappiness hook. You can add happiness text and remarks with localization (See an example in ExampleMod/Localization/en-US.lang).
+			// NOTE: The following code uses chaining - a style that works due to the fact that the SetXAffection methods return the same NPCHappiness instance they're called on.
+			NPC.Happiness
+				.SetBiomeAffection<ForestBiome>(AffectionLevel.Like) // Example Person prefers the forest.
+				.SetBiomeAffection<SnowBiome>(AffectionLevel.Dislike) // Example Person dislikes the snow.
+				.SetBiomeAffection<HallowBiome>(AffectionLevel.Like) // Example Person likes the Example Surface Biome
+				.SetBiomeAffection<DesertBiome>(AffectionLevel.Love)
+				.SetBiomeAffection<JungleBiome>(AffectionLevel.Hate)
+				.SetBiomeAffection<UndergroundBiome>(AffectionLevel.Dislike)
+				.SetBiomeAffection<OceanBiome>(AffectionLevel.Like)
+				.SetNPCAffection(NPCID.Dryad, AffectionLevel.Love) // Loves living near the dryad.
+				.SetNPCAffection(NPCID.Guide, AffectionLevel.Dislike) // Likes living near the guide.
+				.SetNPCAffection(NPCID.Merchant, AffectionLevel.Dislike) // Dislikes living near the merchant.
+				.SetNPCAffection(NPCID.Demolitionist, AffectionLevel.Hate) // Hates living near the demolitionist.
+				.SetNPCAffection(NPCID.Angler, AffectionLevel.Hate)
+				.SetNPCAffection(NPCID.Steampunker, AffectionLevel.Love)
+				.SetNPCAffection(NPCID.BestiaryGirl, AffectionLevel.Love)
+				.SetNPCAffection(NPCID.Mechanic, AffectionLevel.Like)
+				.SetNPCAffection(NPCID.GoblinTinkerer, AffectionLevel.Hate)
+
+			; // < Mind the semicolon!
+
+			// This creates a "profile" for ExamplePerson, which allows for different textures during a party and/or while the NPC is shimmered.
+		}
+
+		public override void SetDefaults()
+		{
+			NPC.townNPC = true; // Sets NPC to be a Town NPC
+			NPC.friendly = true; // NPC Will not attack player
+			NPC.width = 18;
+			NPC.height = 40;
+			NPC.aiStyle = 7;
+			NPC.damage = 15;
+			NPC.defense = 45;
+			NPC.lifeMax = 300;
+			NPC.HitSound = SoundID.NPCHit1;
+			NPC.DeathSound = SoundID.NPCDeath1;
+			NPC.knockBackResist = 0.5f;
+			NPC.frame.Height = 54;
+			NPC.frame.Width = 42;
+
+			AnimationType = NPCID.Guide;
+		}
+
+		public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry)
+		{
+			// We can use AddRange instead of calling Add multiple times in order to add multiple items at once
+			bestiaryEntry.Info.AddRange(new IBestiaryInfoElement[] {
+				// Sets the preferred biomes of this town NPC listed in the bestiary.
+				// With Town NPCs, you usually set this to what biome it likes the most in regards to NPC happiness.
+				BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Biomes.Surface,
+
+				// Sets your NPC's flavor text in the bestiary.
+				new FlavorTextBestiaryInfoElement("Oddly enough, this mysterious terrarian seems to know a lot about shields, although he doesn't use one."),
+
+				// You can add multiple elements if you really wanted to
+				// You can also use localization keys (see Localization/en-US.lang)
+				new FlavorTextBestiaryInfoElement("Mods.ExampleMod.Bestiary.ExamplePerson")
+			});
+		}
+
+		// The PreDraw hook is useful for drawing things before our sprite is drawn or running code before the sprite is drawn
+		// Returning false will allow you to manually draw your NPC
+		public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
+		{
+			// This code slowly rotates the NPC in the bestiary
+			// (simply checking NPC.IsABestiaryIconDummy and incrementing NPC.Rotation won't work here as it gets overridden by drawModifiers.Rotation each tick)
+			if (NPCID.Sets.NPCBestiaryDrawOffset.TryGetValue(Type, out NPCID.Sets.NPCBestiaryDrawModifiers drawModifiers))
+			{
+				drawModifiers.Rotation += 0.001f;
+
+				// Replace the existing NPCBestiaryDrawModifiers with our new one with an adjusted rotation
+				NPCID.Sets.NPCBestiaryDrawOffset.Remove(Type);
+				NPCID.Sets.NPCBestiaryDrawOffset.Add(Type, drawModifiers);
+			}
+
+			return true;
+		}
+
+		public override bool CanTownNPCSpawn(int numTownNPCs)
+		{ // Requirements for the town NPC to spawn.
+			for (int k = 0; k < Main.maxPlayers; k++)
+			{
+				Player player = Main.player[k];
+				if (!player.active)
+				{
+					continue;
+				}
+
+				// Player has to have either an ExampleItem or an ExampleBlock in order for the NPC to spawn
+				if (player.GetModPlayer<ShatteredPlayer>().shieldEquipped)
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		// Example Person needs a house built out of ExampleMod tiles. You can delete this whole method in your townNPC for the regular house conditions.
+
+		public override ITownNPCProfile TownNPCProfile()
+		{
+			return NPCProfile;
+		}
+
+		public override List<string> SetNPCNameList()
+		{
+			return new List<string>() {
+				"Trinity",
+				"Alyx",
+				"Peyton",
+				"Kye",
+				"Aeron"
+			};
+		}
+
+		public override string GetChat()
+		{
+			WeightedRandom<string> chat = new WeightedRandom<string>();
+
+			int Steampunker = NPC.FindFirstNPC(NPCID.Steampunker);
+			if (Steampunker >= 0 && Main.rand.NextBool(4))
+			{
+				chat.Add("I can really talk to" + Main.npc[Steampunker].GivenName + ". Once, we talked for five hours. Wow..");
+			}
+			int Zoologist = NPC.FindFirstNPC(NPCID.BestiaryGirl);
+			if (Steampunker >= 0 && Main.rand.NextBool(4))
+			{
+				chat.Add("I really like how" + Main.npc[Zoologist].GivenName + "looks today. Did she say anything about me today? No? Figures.");
+			}
+			// These are things that the NPC has a chance of telling you when you talk to it.
+			chat.Add("Hey, stop bugging me already, just take watcha need and leave.");
+			chat.Add("Honestly, without my goods, you're not gonna survive the next night!");
+			chat.Add("Oh it's you...at least you'll buy something, right? RIGHT?");
+			chat.Add("I would prefer you to buy something, but if you ask about shields, I'll talk my head off!");
+			chat.Add("Am I enjoying your company? I'd rather not say...sorry.");
+
+			NumberOfTimesTalkedTo++;
+			if (NumberOfTimesTalkedTo >= 10)
+			{
+				//This counter is linked to a single instance of the NPC, so if ExamplePerson is killed, the counter will reset.
+				chat.Add(Language.GetTextValue("For the MILLIONTH TIME, LEAVE ME ALONE ALREADY."));
+				NumberOfTimesTalkedTo = 0;
+			}
+
+			string chosenChat = chat; // chat is implicitly cast to a string. This is where the random choice is made.
+
+			return chosenChat;
+		}
+
+		public override void SetChatButtons(ref string button, ref string button2)
+		{ // What the chat buttons are when you open up the chat UI
+			button = Language.GetTextValue("Shop");
+			button2 = "Shields Info";
+		}
+
+		public override void OnChatButtonClicked(bool firstButton, ref string shop)
+		{
+			if (firstButton)
+			{
+				// We want 3 different functionalities for chat buttons, so we use HasItem to change button 1 between a shop and upgrade action.
+				shop = ShopName; // Name of the shop tab we want to open.
+			}
+		}
+
+		// Not completely finished, but below is what the NPC will sell
+		public override void AddShops()
+		{
+			var npcShop = new NPCShop(Type, ShopName)
+				.Add(new Item(ModContent.ItemType<WoodShield>())); // This example sets a custom price, ExampleNPCShop.cs has more info on custom prices and currency. 
+			npcShop.Register(); // Name of this shop tab
+		}
+
+		public override void ModifyActiveShop(string shopName, Item[] items)
+		{
+			foreach (Item item in items)
+			{
+				// Skip 'air' items and null items.
+				if (item == null || item.type == ItemID.None)
+				{
+					continue;
+				}
+
+				// If NPC is shimmered then reduce all prices by 50%.
+				if (NPC.IsShimmerVariant)
+				{
+					int value = item.shopCustomPrice ?? item.value;
+					item.shopCustomPrice = value / 2;
+				}
+			}
+		}
+
+		public override void ModifyNPCLoot(NPCLoot npcLoot)
+		{
+			npcLoot.Add(ItemDropRule.Common(ModContent.ItemType<WoodShield>(), 5));
+		}
+
+		// Make this Town NPC teleport to the King and/or Queen statue when triggered. Return toKingStatue for only King Statues. Return !toKingStatue for only Queen Statues. Return true for both.
+		public override bool CanGoToStatue(bool toKingStatue) => true;
+
+		// Make something happen when the npc teleports to a statue. Since this method only runs server side, any visual effects like dusts or gores have to be synced across all clients manually.
+
+		// Create a square of pixels around the NPC on teleport.
+		public void StatueTeleport()
+		{
+			for (int i = 0; i < 30; i++)
+			{
+				Vector2 position = Main.rand.NextVector2Square(-20, 21);
+				if (Math.Abs(position.X) > Math.Abs(position.Y))
+				{
+					position.X = Math.Sign(position.X) * 20;
+				}
+				else
+				{
+					position.Y = Math.Sign(position.Y) * 20;
+				}
+
+			}
+		}
+
+		public override void TownNPCAttackStrength(ref int damage, ref float knockback)
+		{
+			damage = 20;
+			knockback = 4f;
+		}
+
+		public override void TownNPCAttackCooldown(ref int cooldown, ref int randExtraCooldown)
+		{
+			cooldown = 1;
+			randExtraCooldown = 15;
+		}
+
+		public override void TownNPCAttackProj(ref int projType, ref int attackDelay)
+		{
+			projType = ProjectileID.WoodenArrowFriendly;
+			attackDelay = 1;
+		}
+
+		public override void TownNPCAttackProjSpeed(ref float multiplier, ref float gravityCorrection, ref float randomOffset)
+		{
+			multiplier = 12f;
+			randomOffset = 2f;
+			// SparklingBall is not affected by gravity, so gravityCorrection is left alone.
+		}
+
+		// Let the NPC "talk about" minion boss
+
+	}
+}
